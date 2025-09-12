d87fc8d6acefdf0bf90286a5028d816663c49622
nolifeplayer <KAig19@nycstudents.net>
2025-09-12 12:40:27 -0400

    Scripts

--- Assets/C#Script/FollowPlayer.cs
+++ Assets/C#Script/FollowPlayer.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    public GameObject player;
+    private Vector3 offset = new Vector3 (0, 5, -7);
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
