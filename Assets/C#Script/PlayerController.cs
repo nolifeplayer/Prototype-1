dce53a77b49ff3133e7e266a6845dedfb36feae0
nolifeplayer <KAig19@nycstudents.net>
2025-09-16 12:14:58 -0400

    Player Movement

--- Assets/C#Script/PlayerController.cs
+++ Assets/C#Script/PlayerController.cs
@@ -9,11 +9,21 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-    public float speed = 20.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Update is called once per frame
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
