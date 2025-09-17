ee4eae6d845960172596b1a0ec04614996ffc0de
nolifeplayer <KAig19@nycstudents.net>
2025-09-17 12:12:47 -0400

    Plane  Challenge

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3 (42, 0, 25);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+   
+   
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private float turnSpeed = 60.0f;
+    private float forwardInput;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        forwardInput = Input.GetAxis("Vertical");
+        
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.left, turnSpeed * forwardInput * Time.deltaTime);
+    }
+}
